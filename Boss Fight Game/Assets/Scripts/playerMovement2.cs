8a478224ed873808c55fd009ec1543921087aafb
BHASVIC-Thomassmyth23 <145350806+BHASVIC-Thomassmyth23@users.noreply.github.com>
2024-10-16 08:44:57 +0100

    Update playerMovement2.cs

--- Boss Fight Game/Assets/Scripts/playerMovement2.cs
+++ Boss Fight Game/Assets/Scripts/playerMovement2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class playerMovement2 : MonoBehaviour
 {
-    float speed = 3f;
+    public float speed = 3f;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,4 +24,5 @@ public class playerMovement2 : MonoBehaviour
 
         transform.position = pos; //performs the movement transformation
     } 
-}|
+    
+}
